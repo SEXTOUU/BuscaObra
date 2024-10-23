1c2000cd1f2b0f2e03dbc0d0bb6acbddce851e06
BuscaObra <geninetemm@gmail.com>
2024-10-22 21:38:18 -0300

    Delete BUSCA OBRA/BUSCA OBRA directory

--- BUSCA OBRA/BUSCA OBRA/TELAS DE INICIO/HTML/JS/administrador.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.remove-btn').forEach(button => {
-    button.addEventListener('click', function () {
-      this.closest('tr').remove();
-      alert('Usuário removido com sucesso!');
-    });
-  });
-  
