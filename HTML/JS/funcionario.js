233c1cd37a0e29208716622864aafd8402d4f04e
BuscaObra <geninetemm@gmail.com>
2024-10-22 21:38:07 -0300

    Delete HTML directory

--- HTML/JS/funcionario.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.status-btn').addEventListener('click', function () {
-    const status = this.textContent === 'Pendente' ? 'Concluído' : 'Pendente';
-    this.textContent = status;
-    this.style.backgroundColor = status === 'Concluído' ? '#5cb85c' : '#f0ad4e';
-  });
